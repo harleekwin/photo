2d6d4754ed7e038945905686864f0c2ec344e3a2
Mary White <mwhite2503@gmail.com>
2017-05-26 14:31:53 -0700

    update

--- ngApp/controllers/controllers.ts
+++ ngApp/controllers/controllers.ts
@@ -2,6 +2,7 @@ namespace myapp.Controllers {
 
     export class HomeController {
         public file;
+        public productToSave;
 
         public pickFile() {
           this.filepickerService.pick(
@@ -12,6 +13,7 @@ namespace myapp.Controllers {
 
         public fileUploaded(file) {
           this.file = file;
+          this.productToSave.url = this.file.url;
           this.$scope.$apply();
         }
 
@@ -19,30 +21,4 @@ namespace myapp.Controllers {
       }
 
       angular.module('myapp').controller('HomeController', HomeController);
-
-
-    export class AboutController {
-        public message = 'Hello from the about page!';
-    }
-
-    export class EditController {
-      public file;
-
-      public save() {
-        this.photoService.save(this.file.then(() => {
-          this.$state.go('home');
-        });
-      }
-
-      constructor(
-        private photoService:myapp.Services.PhotoService,
-        private $state:ng.ui.IStateService,
-        private $stateParams:ng.ui.IStateParamsService
-      ) {
-        let fieldId = $stateParams['id'];
-        this.file = photoService.get(fileId);
-      }
-
-    }
-
 }
