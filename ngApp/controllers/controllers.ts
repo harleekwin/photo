4a6601b9578256195304c4f1c9fca7bda9ba9a1e
Mary White <mwhite2503@gmail.com>
2017-05-26 13:52:34 -0700

    sample

---
+++ ngApp/controllers/controllers.ts
@@ -0,0 +1,48 @@
+namespace myapp.Controllers {
+
+    export class HomeController {
+        public file;
+
+        public pickFile() {
+          this.filepickerService.pick(
+            { mimetype: 'image/*' },
+            this.fileUploaded.bind(this)
+          );
+        }
+
+        public fileUploaded(file) {
+          this.file = file;
+          this.$scope.$apply();
+        }
+
+        constructor(private filepickerService, private $scope: ng.IScope) { }
+      }
+
+      angular.module('myapp').controller('HomeController', HomeController);
+
+
+    export class AboutController {
+        public message = 'Hello from the about page!';
+    }
+
+    export class EditController {
+      public file;
+
+      public save() {
+        this.photoService.save(this.file.then(() => {
+          this.$state.go('home');
+        });
+      }
+
+      constructor(
+        private photoService:myapp.Services.PhotoService,
+        private $state:ng.ui.IStateService,
+        private $stateParams:ng.ui.IStateParamsService
+      ) {
+        let fieldId = $stateParams['id'];
+        this.file = photoService.get(fileId);
+      }
+
+    }
+
+}
